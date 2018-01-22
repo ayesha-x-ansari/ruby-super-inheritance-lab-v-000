0448f033bac018e70e6eb406bbe08771e7153fc8
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-22 04:55:51 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -10,9 +10,9 @@ class Student
 end
 
 
-class ChattyStudent
+class ChattyStudent < Student
 
-  def hello < student
+  def hello 
     super
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man,
     it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
